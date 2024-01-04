e0db21b39bc3c6bd3745a51225a9a1bc04c77ad1
cz <1980103789@qq.com>
2024-01-04 19:36:38 +0800

    feat: 语法分析树建构完成

---
+++ src/lex.py
@@ -0,0 +1,159 @@
+import ply.lex as lex
+import sys
+# Define the tokens
+tokens = (
+    'ID',              # 标识符
+    'NUMBER',          # 数字
+    'STRING',          # 字符串
+    'ASSIGN',          # 赋值
+    'UNARY_OP',        # 单目运算
+    'BINARY_OP',       # 双目运算
+    'COMPARISON_OP',   # 比较运算
+    'AND_OP',          # 且
+    'OR_OP',           # 或
+    'NO_OP',           # 非
+    'SEMICOLON',       # 分号
+    'ELLIPSIS',     # 省略号
+    'COLON',        # 冒号
+    'COMMA',        # 逗号
+    'EQUAL',        # 等号
+    'PERIOD',       # 句点
+    'AMPERSAND',    # 与号
+    'EXCLAMATION',  # 感叹号
+    'SQUARE_BRACKETS_LEFT',   # 方括号
+    'SQUARE_BRACKETS_RIGHT',  # 方括号
+    'CURLY_BRACES_LEFT',      # 大括号
+    'CURLY_BRACES_RIGHT',     # 大括号
+    'TILDE',        # 波浪号
+    'PARENTHESES_LEFT',   # 括号
+    'PARENTHESES_RIGHT',  # 括号
+    'PLUS',         # 加号
+    'MINUS',        # 减号
+    'ASTERISK',     # 星号
+    'SLASH',        # 斜杠
+    'PERCENT',      # 百分号
+    'GREATER_THAN', # 大于号
+    'LESS_THAN',    # 小于号
+    'CARET',        # 异或符号
+    'PIPE',         # 竖线
+    'QUESTION_MARK', # 问号
+    'HASH',          # 井号
+)
+
+reversed = {
+    'int': 'INT',
+    'float': 'FLOAT',
+    'char': 'CHAR',
+    'void': 'VOID',
+    'double': 'DOUBLE',
+    'long': 'LONG',
+    'const': 'CONST',
+    'if': 'IF',
+    'else': 'ELSE',
+    'while': 'WHILE',
+    'for': 'FOR',
+    'do': 'DO',
+    'return': 'RETURN',
+    'break': 'BREAK',
+    'continue': 'CONTINUE',
+    'switch': 'SWITCH',
+    'case': 'CASE',
+    'default': 'DEFAULT',
+    'include': 'INCLUDE',
+    'false': 'FALSE',
+    'true': 'TRUE',
+    'bool':'BOOL',
+}
+
+tokens += tuple(reversed.values())
+
+t_ELLIPSIS = r'\.\.\.'
+t_SEMICOLON = r';'
+t_ASSIGN = r'>>=|<<=|\+=|-=|\*=|/=|%=|&=|\^=|\\'
+t_UNARY_OP = r'--|\+\+'
+t_BINARY_OP = r'<<|>>'
+t_COMPARISON_OP = r'<=|>=|==|!='
+t_AND_OP = r'&&'
+t_OR_OP = r'\|\|'
+t_NO_OP = r'!'
+
+t_COLON = r':'
+t_COMMA = r','
+t_EQUAL = r'='
+t_PERIOD = r'\.'
+t_AMPERSAND = r'&'
+t_EXCLAMATION = r'!'
+t_SQUARE_BRACKETS_LEFT = r'\['
+t_SQUARE_BRACKETS_RIGHT = r'\]'
+t_CURLY_BRACES_LEFT = r'\{'
+t_CURLY_BRACES_RIGHT = r'\}'
+t_TILDE = r'~'
+t_PARENTHESES_LEFT = r'\('
+t_PARENTHESES_RIGHT = r'\)'
+t_PLUS = r'\+'
+t_MINUS = r'\-'
+t_ASTERISK = r'\*'
+t_SLASH = r'\\'
+t_PERCENT = r'%'
+t_GREATER_THAN = r'>'
+t_LESS_THAN = r'<'
+t_CARET = r'\^'
+t_PIPE = r'\|'
+t_QUESTION_MARK = r'\?'
+t_HASH = r'\#'
+
+# 变量名、函数名等标识符，除去保留字
+def t_ID(token):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    token.type = reversed.get(token.value, 'ID')
+    return token
+
+# 整数，十六进制证书，浮点数
+def t_NUMBER(token):
+    r'0[xX][0-9a-fA-F]+|\d+\.\d+|\d+'
+    if token.value.startswith('0x'):
+        token.value = int(token.value, 16)
+    elif '.' in token.value:
+        token.value = float(token.value)
+    else:
+        token.value = int(token.value)
+    return token
+
+def t_STRING(token):
+    r'\"[^"\n]*\"'
+    return token
+
+def t_ANNOTATION(token):
+    r'//[^\n]*'
+    pass
+# Add more token definitions here
+
+# 过滤空格换行
+t_ignore = ' \t\r\n'
+
+# Define error handling rule
+def t_error(token):
+    print("Illegal character '%s'" % token.value[0])
+    token.lexer.skip(1)
+
+# Build the lexer
+lexer = lex.lex()
+if __name__ == '__main__':
+    file_path = "./huiwen.cpp"
+    arguments = sys.argv[1:]
+    if len(arguments) > 0:
+        file_path = arguments[0]
+
+        
+    
+    with open(file_path, "r") as file:
+        data = file.read()
+
+    lexer.input(data)
+    with open("output.txt", "w") as file:
+        while True:
+            token = lexer.token()
+            if not token:
+                break
+            print(token, file=file)
+
--- src/yacc.py
+++ src/yacc.py
@@ -1,27 +1,34 @@
 import ply.yacc as yacc
 from lex import tokens
+from AST import InternalNode,ExternalNode
+
+
 # 程序由不同的声明组成，声明可以是变量声明或函数声明
 def p_program(p):
     '''program : external_declaration program
                | external_declaration'''
     print("program->")
+    p[0]=InternalNode('program',p[1:])
 
 # 声明可以是函数声明和变量等声明
 def p_external_declaration(p):
     '''external_declaration : function_declaration
                             | declaration_statement'''   
     print("external_declaration->")
+    p[0]=InternalNode('external_declaration',p[1:])
 
 # 函数声明：返回类型 声明 函数体 
 def p_function_declaration(p):
     '''function_declaration : declaration_specifiers func_declarator compound_statement'''
     print("function_declaration->")
+    p[0]=InternalNode('function_declaration',p[1:])
 
 # 返回类型：（const） 类型
 def p_declaration_specifiers(p):
     '''declaration_specifiers : CONST type_specifier
                               | type_specifier'''
     print("declaration_specifiers->",p[1])
+    p[0]=InternalNode('declaration_specifiers',p[1:])
 
 # 类型
 def p_type_specifier(p):
@@ -33,12 +40,14 @@ def p_type_specifier(p):
                       | LONG
                       | BOOL'''
     print("type_specifier->",p[1])
+    p[0]=InternalNode('type_specifier',p[1:])
 
 # 函数声明 : 指针 函数声明体
 def p_func_declarator(p):
     '''func_declarator : ASTERISK direct_declarator 
                         | direct_declarator'''
     print("func_declarator->")
+    p[0]=InternalNode('func_declarator',p[1:])
 
 # 函数声明体 : 标识符(空或参数)
 def p_direct_declarator(p):
@@ -46,17 +55,22 @@ def p_direct_declarator(p):
 	                    | ID PARENTHESES_LEFT PARENTHESES_RIGHT
                         '''
     print("direct_declarator->",p[1])
+    p[0]=InternalNode('direct_declarator',p[1:])
 
 # 参数列表 : 参数列表,参数声明 | 参数声明
 def p_parameter_list(p):
     '''parameter_list : parameter_list COMMA parameter_declaration
                       | parameter_declaration'''
     print("parameter_list->")
+    p[0]=InternalNode('parameter_list',p[1:])
+
 
 # 参数声明 : 类型 参数声明体
 def p_parameter_declaration(p):
     '''parameter_declaration : declaration_specifiers declarator'''
     print("parameter_declaration->")
+    p[0]=InternalNode('parameter_declaration',p[1:])
+
 
 # 参数声明体 : 指针 标识符 | 标识符[] | 标识符
 def p_declarator(p):
@@ -65,18 +79,24 @@ def p_declarator(p):
                 | ID SQUARE_BRACKETS_LEFT expression SQUARE_BRACKETS_RIGHT
                 | ASTERISK ID'''
     print("declarator->",p[1])
+    p[0]=InternalNode('declarator',p[1:])
+
 
 # 函数体 : { (语句列表) }
 def p_compound_statement(p):
     '''compound_statement : CURLY_BRACES_LEFT CURLY_BRACES_RIGHT
                           | CURLY_BRACES_LEFT statement_list CURLY_BRACES_RIGHT'''
     print("compound_statement->")
+    p[0]=InternalNode('compound_statement',p[1:])
+
 
 # 语句列表 : 语句列表 语句 | 语句
 def p_statement_list(p):
     '''statement_list : statement_list statement
                       | statement'''
     print("statement_list->")
+    p[0]=InternalNode('statement_list',p[1:])
+
 
 # 语句 : 声明 ｜ 函数 ｜ 表达式 ｜ 选择语句 ｜ 迭代语句 ｜ 跳转语句 | 赋值语句
 def p_statement(p):
@@ -86,22 +106,30 @@ def p_statement(p):
                  | iteration_statement
                  | jump_statement
                  | assignment_statement'''
+    print("statement->")
+    p[0]=InternalNode('statement',p[1:])
+    
 
 # 声明语句 : 声明 ;
 def p_declaration_statement(p):
     '''declaration_statement : declaration SEMICOLON'''
     print("declaration_statement->")
+    p[0]=InternalNode('declaration_statement',p[1:])
+
 
 # 声明 : 类型 标识符
 def p_declaration(p):
     '''declaration : declaration_specifiers init_declarator_list'''
     print("declaration->")
+    p[0]=InternalNode('declaration',p[1:])
+
 
 # 初始化声明列表 : 初始化声明列表 , 初始化声明 | 初始化声明
 def p_init_declarator_list(p):
     '''init_declarator_list : init_declarator_list COMMA init_declarator
                             | init_declarator'''
     print("init_declarator_list->")
+    p[0]=InternalNode('init_declarator_list',p[1:])
 
 
 
@@ -111,23 +139,31 @@ def p_init_declarator(p):
                        | ID EQUAL expression
                        | ID SQUARE_BRACKETS_LEFT expression SQUARE_BRACKETS_RIGHT'''
     print("init_declarator->")
+    p[0]=InternalNode('init_declarator',p[1:])
+
 
 # 表达式语句 : 表达式 ｜ 赋值表达式;
 def p_expression_statement(p):
     '''expression_statement : expression SEMICOLON'''
     print("expression_statement->")
+    p[0]=InternalNode('expression_statement',p[1:])
+
     
 # 赋值语句 : 标识符([表达式]) 赋值运算符 表达式
 def p_assignment_statement(p):
     '''assignment_statement : ID assignment_operator expression SEMICOLON
                             | ID array_index assignment_operator expression SEMICOLON'''
     print("assignment_statement->")
+    p[0]=InternalNode('assignment_statement',p[1:])
+
 
 # 赋值运算符 : = | += | -= | *= | /= | %= | &= | ^= | |= | <<= | >>=
 def p_assignment_operator(p):
     '''assignment_operator : EQUAL
                            | ASSIGN'''
     print("assignment_operator->")
+    p[0]=InternalNode('assignment_operator',p[1:])
+
 
 # 表达式 ： 运算单元 | 运算单元 运算符 表达式
 def p_expression(p):
@@ -137,7 +173,9 @@ def p_expression(p):
                   | unary_expression mutiplicative_operator expression
                   | unary_expression additive_operator expression
                   | unary_expression bitwise_operator expression'''
-    print("expression->")   
+    print("expression->") 
+    p[0]=InternalNode('expression',p[1:])
+    
 
 # 乘法运算符 : * | / | %
 def p_mutiplicative_operator(p):
@@ -145,12 +183,14 @@ def p_mutiplicative_operator(p):
                               | SLASH
                               | PERCENT'''
     print("mutiplicative_operator->")
+    p[0]=InternalNode('mutiplicative_operator',p[1:])
 
 # 加法运算符 : + | -
 def p_additive_operator(p):
     '''additive_operator : PLUS
                          | MINUS'''
     print("additive_operator->")
+    p[0]=InternalNode('additive_operator',p[1:])
 
 # 位运算符 :  & | ^ | |
 def p_bitwise_operator(p):
@@ -158,6 +198,7 @@ def p_bitwise_operator(p):
                         | CARET
                         | PIPE'''
     print("bitwise_operator->")
+    p[0]=InternalNode('bitwise_operator',p[1:])
 
 # 运算单元 : 单元表达式 ｜ 单目运算符 单元表达式
 def p_unary_expression(p):
@@ -165,6 +206,7 @@ def p_unary_expression(p):
                         | prefix_unary_operator primary_expression
                         | primary_expression'''
     print("unary_expression->")
+    p[0]=InternalNode('unary_expression',p[1:])
 
 # 前置单目运算符 : ++ | -- | * | ~ | ! | &
 def p_prefix_unary_operator(p):
@@ -174,11 +216,13 @@ def p_prefix_unary_operator(p):
                              | EXCLAMATION
                              | AMPERSAND'''
     print("prefix_unary_operator->")
+    p[0]=InternalNode('prefix_unary_operator',p[1:])
 
 # 后置单目运算符 : ++ | -- 
 def p_postfix_unary_operator(p):
     '''postfix_unary_operator : UNARY_OP'''
     print("postfix_unary_operator->")
+    p[0]=InternalNode('postfix_unary_operator',p[1:])
 
 # 单元表达式 : 标识符 | 数字 | 字符串 | (表达式) ｜ 函数调用 | 数组访问 
 def p_primary_expression(p):
@@ -190,23 +234,28 @@ def p_primary_expression(p):
                           | ID array_index
                           '''
     print("primary_expression->")
+    p[0]=InternalNode('primary_expression',p[1:])
+
 
 # 函数调用 : 标识符(参数列表)
 def p_function_call(p):
     '''function_call : ID PARENTHESES_LEFT call_parameter_list PARENTHESES_RIGHT
                      | ID PARENTHESES_LEFT PARENTHESES_RIGHT'''
     print("function_call->")
+    p[0]=InternalNode('function_call',p[1:])
 
 # 调用参数列表 : 调用参数列表,调用参数 | 调用参数
 def p_call_parameter_list(p):
     '''call_parameter_list : call_parameter_list COMMA call_parameter
                            | call_parameter'''
     print("call_parameter_list->")
+    p[0]=InternalNode('call_parameter_list',p[1:])
 
 # 调用参数 : 表达式
 def p_call_parameter(p):
     '''call_parameter : expression'''
     print("call_parameter->")
+    p[0]=InternalNode('call_parameter',p[1:])
 
 # 数组访问 : 标识符[表达式]([表达式])
 def p_array_index(p):
@@ -214,6 +263,7 @@ def p_array_index(p):
                     | SQUARE_BRACKETS_LEFT expression SQUARE_BRACKETS_RIGHT'''
 
     print("array_index->")
+    p[0]=InternalNode('array_index',p[1:])
 
 # 条件：IF (表达式) 语句 | IF (表达式) 语句 ELSE 语句 | SWITCH (表达式) 语句
 def p_selection_statement(p):
@@ -221,6 +271,7 @@ def p_selection_statement(p):
                            | IF PARENTHESES_LEFT expression PARENTHESES_RIGHT compound_statement ELSE compound_statement
                            | SWITCH PARENTHESES_LEFT expression PARENTHESES_RIGHT compound_statement'''
     print("selection_statement->")
+    p[0]=InternalNode('selection_statement',p[1:])
 
 # 迭代：WHILE (表达式) 语句 | DO 语句 WHILE (表达式) | FOR (表达式;表达式;) 语句 | FOR (表达式;表达式;表达式) 语句
 def p_iteration_statement(p):
@@ -229,16 +280,15 @@ def p_iteration_statement(p):
                            | FOR PARENTHESES_LEFT expression_statement expression_statement PARENTHESES_RIGHT compound_statement
                            | FOR PARENTHESES_LEFT expression_statement expression_statement expression PARENTHESES_RIGHT compound_statement'''
     print("iteration_statement->")
+    p[0]=InternalNode('iteration_statement',p[1:])
     
 # 跳转：RETURN 表达式 ; | BREAK ; | CONTINUE ; | GOTO 标识符 ; | GOTO * 表达式 ; | GOTO ** 表达式 ;
 def p_jump_statement(p):
     '''jump_statement : RETURN expression SEMICOLON
                       | BREAK SEMICOLON
-                      | CONTINUE SEMICOLON
-                      | GOTO ID SEMICOLON
-                      | GOTO ASTERISK expression SEMICOLON
-                      | GOTO ASTERISK ASTERISK expression SEMICOLON'''
+                      | CONTINUE SEMICOLON'''
     print("jump_statement->")
+    p[0]=InternalNode('jump_statement',p[1:])
 
 
 # 错误处理
@@ -256,7 +306,11 @@ if __name__ == '__main__':
             with open(filename, 'r') as file:
                 code = file.read()
             result = parser.parse(code)
-            print(result)
+            output_filename = "output.yaml"
+            with open(output_filename, 'w') as output_file:
+                output_file.write(result.to_yaml())
+            #print(result.to_yaml())
+            #print(result)
         except EOFError:
             break
     
